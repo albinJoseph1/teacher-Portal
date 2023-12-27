1fdd2afc723634fc71bf6cc865202cb5b6a7c8b6
Albin Joseph <albin.joseph@yatnam.com>
2023-12-27 15:24:12 +0530

    Add Config.example And Connection Page

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'albin');
+define('DB_PASSWORD', 'start');
+define('DB_NAME', 'classroom');
+?>
--- connection.php
+++ connection.php
@@ -1,3 +1,9 @@
 <?php
-include('config.php');    
+    if (!file_exists('config.php')){
+        die('Error: config.php file not found. Please create the config.php file.');
+    }
+    else{
+        include("config.php");
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    }
 ?>
